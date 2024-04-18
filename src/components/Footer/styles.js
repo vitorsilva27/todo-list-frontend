499edadc5bb2657100c3e70d9b505585829084eb
Vitor Gabriel <vg285757@gmail.com>
2024-04-18 19:37:54 -0300

    Creating Footer  Component

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 50px;
+  background: #171717;
+  border-top: 5px solid #e6cf09;
+
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  span {
+    color: #fff;
+  }
+`;
